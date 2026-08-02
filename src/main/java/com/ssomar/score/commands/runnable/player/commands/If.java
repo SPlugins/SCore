677057758bd61363aa7c98bcadc4e447eb0afb88
Josef Miko Urquico <josef.urquico@gmail.com>
2026-08-02 20:38:52 +0800

    opted for a different way of checking

--- src/main/java/com/ssomar/score/commands/runnable/player/commands/If.java
+++ src/main/java/com/ssomar/score/commands/runnable/player/commands/If.java
@@ -92,7 +92,10 @@ public class If extends PlayerCommand {
                 StringBuilder cond = new StringBuilder();
                 while (i < condition.length() && condition.charAt(i) != '(' && condition.charAt(i) != ')' && condition.charAt(i) != '&') {
                     // SCore's %rand% placeholder uses '|' to separate number ranges, so extra logic is required to properly detect "||" and "&&" char sequences
-                    if (i+1 < condition.length() && (condition.substring(i, i+2).equals("||") || condition.substring(i, i+2).equals("&&"))) {
+                    char c = condition.charAt(i);
+                    char next = (i + 1 < condition.length()) ? condition.charAt(i + 1) : '\0';
+
+                    if ((c == '|' && next == '|') || (c == '&' && next == '&')) {
                         break;
                     }
                     cond.append(condition.charAt(i));
